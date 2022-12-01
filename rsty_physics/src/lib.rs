481184ef8ae3fb937c24be35936d293e6b41b644
thebigG <yunior.eury@gmail.com>
2022-11-30 22:55:00 -0600

    -Cleanup

--- rsty_physics/src/lib.rs
+++ rsty_physics/src/lib.rs
@@ -3,9 +3,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
-
-#![feature(cfg_eval)]
-#![feature(type_name_of_val)]
 use godot::engine::utilities::sin;
 use godot::prelude::*;
 
