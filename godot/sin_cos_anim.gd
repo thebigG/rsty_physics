c024787aa1aae0666ad87ae27cc48e0cf2d0d3fd
thebigG <yunior.eury@gmail.com>
2022-12-31 02:16:12 -0500

    -Animate in a circle using sin and cos

--- godot/sin_cos_anim.gd
+++ godot/sin_cos_anim.gd
@@ -1,6 +1,7 @@
 extends Main
 
 #TODO: Add curves. Play with radius, x, y, etc
+#Animates Godot in a circle using cos/x, sin/y and radius around a center_x,center_y 
 #This draws a cos and sin wave.
 #At the moment SineWave is just a Line2D node.
 #This will probably stay like this until https://github.com/godot-rust/gdextension/issues/55
@@ -10,20 +11,26 @@ var sin_wave = SineWave2D.new();
 var cos_wave = Line2D.new();
 
 var y_axis = Line2D.new();
-var x_axis = Line2D.new();
+var x_axis = Line2D.new();	
 
 var Origin = Vector2(300,200);
 
 var obj = Sprite2D.new()
 
 var current_sinusoidal_output_val = 0
-var sin_step = 0.1  #radians
+var current_cos_output_val = 0
+var sin_step = 0.1  #radians/spped/rate
 var current_sin_input_val = 0
-var sin_output_scale = 50
+var output_scale = 50
 
 var sin_output_offset = Origin.y  # Added to sin_output
 var sin_label = Label.new()
 var cos_label = Label.new()
+
+var center_x = Origin.x
+var center_y = Origin.y
+
+var radius = 100
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -83,9 +90,18 @@ func get_cos_full_circle_2dvectors(degrees_delta: int, scale: int, number_of_pha
 		i += deg_to_rad(degrees_delta)
 	return points
 
+func curve():
+	pass
+
 func _physics_process(delta):
-	current_sinusoidal_output_val = sin(current_sin_input_val) * sin_output_scale
-	obj.position.y = current_sinusoidal_output_val + sin_output_offset
+	current_sinusoidal_output_val = sin(current_sin_input_val) 
+	current_cos_output_val = cos(current_sin_input_val) 
+	
+	obj.position.x = center_x
+	obj.position.y = center_y
+	obj.position.y += (radius * current_sinusoidal_output_val)  
+	obj.position.x += (radius * current_cos_output_val)  
+	
 	current_sin_input_val += sin_step
 	
 func _process(delta):
