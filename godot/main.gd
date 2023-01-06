5bfd940a8e5bf0dbf42dfbb6633a7f3cf6df282e
thebigG <yunior.eury@gmail.com>
2023-01-05 22:41:22 -0600

    -Add dropdown for selecting views

--- godot/main.gd
+++ godot/main.gd
@@ -1,8 +1,23 @@
 extends Main
 
+var views: OptionButton = OptionButton.new()
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	views.position.x += self.get_viewport().size.y/2
+	for child in self.get_children():
+		views.add_item(child.name)
 	
+	views.item_selected.connect(select_view)
+	add_child(views)
+
+func select_view(selection: int):
+	print(views.get_item_text(selection))
+	for child in get_children():
+		if child.name != views.name:
+			child.set("visible", false)
+			if child.name ==  views.get_item_text(selection):
+				child.set("visible", true)	
+
 func _process(delta):
 	pass
