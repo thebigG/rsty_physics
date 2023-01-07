793c1ddcd68f5c66365c67859210cb07da270ad0
thebigG <yunior.eury@gmail.com>
2023-01-06 23:56:14 -0600

    -Add lissajous curve.

---
+++ godot/sin_cos_curve_lissajous_curve.gd
@@ -0,0 +1,200 @@
+extends Main
+
+#TODO: Add curves. Play with radius, x, y, etc
+#Animates Godot in a circle using cos/x, sin/y and radius around a center_x,center_y 
+#This draws a cos and sin wave.
+#At the moment SineWave is just a Line2D node.
+#This will probably stay like this until https://github.com/godot-rust/gdextension/issues/55
+#is resolved. Then all this code will be moved into
+#a rust gdextension; that it is  modular and scalable.
+var sin_wave = SineWave2D.new();
+var cos_wave = Line2D.new();
+
+var y_axis = Line2D.new();
+var x_axis = Line2D.new();	
+
+#Add Control UI for origin and center_x, center_y
+var origin = Vector2(400,200);
+
+var obj = Sprite2D.new()
+
+var current_sinusoidal_output_val = 0
+var current_cos_output_val = 0
+var x_speed = 0.05  #radians/spped/rate
+var y_speed = 0.05  #radians/spped/rate
+var current_sin_input_val = 0
+var current_cos_input_val = 0
+var output_scale = 50
+
+var sin_output_offset = origin.y  # Added to sin_output
+var sin_label = Label.new()
+var cos_label = Label.new()
+
+
+var center_x = origin.x
+var center_y = origin.y
+
+var x_speed_spinner = SpinBox.new()
+var y_speed_spinner = SpinBox.new()
+var x_radius_spinner = SpinBox.new()
+var y_radius_spinner = SpinBox.new()
+
+
+var x_speed_label = Label.new()
+var y_speed_label = Label.new()
+var x_radius_label = Label.new()
+var y_radius_label = Label.new()
+
+#Add these fields as Control nodes
+var x_radius = 100
+var y_radius = 150
+
+var position_delta = Vector2()
+
+var spinner_x_offset = 200
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var image = Image.load_from_file("res://icon.svg")
+	var texture = ImageTexture.create_from_image(image)
+	
+	obj.texture = texture
+	obj.position = origin
+	obj.scale = Vector2(0.3,0.3)
+	
+	y_axis.default_color = Color(Color.YELLOW)
+	y_axis.default_color.a = 0.25
+	
+	y_axis.points = [Vector2(origin.x, origin.y-100), Vector2(origin.x, origin.y+300)]
+	
+	x_axis.default_color = Color(Color.YELLOW)
+	x_axis.default_color.a = 0.25
+	
+	x_axis.points = [Vector2(origin.x-100, origin.y), Vector2(origin.x+300, origin.y)]
+	
+	sin_wave.default_color = Color.PURPLE
+	super.new_game()
+	sin_wave.points = get_sin_full_circle_2dvectors(30, 50, 2)
+	sin_wave.position = origin
+
+	cos_wave.points = get_cos_full_circle_2dvectors(30, 50, 2)
+	cos_wave.position = origin
+	
+	sin_label.text = "Sin"
+	cos_label.text = "Cos"
+	cos_label.position.y += 25
+	sin_label.add_theme_color_override("font_color", sin_wave.default_color)
+	
+	x_speed_spinner.position.y += 50
+	x_speed_spinner.step = .01
+	
+	y_speed_spinner.position.y += x_speed_spinner.position.y + 30
+	y_speed_spinner.step = .01
+	
+	x_speed_label.text = "X Speed/Angle(Radians):"
+	y_speed_label.text = "Y Speed/Angle(Radians):"
+	
+	x_speed_label.position.y = x_speed_spinner.position.y
+	y_speed_label.position.y = y_speed_spinner.position.y
+	
+	x_speed_spinner.position.x += spinner_x_offset
+	y_speed_spinner.position.x += spinner_x_offset
+	
+	x_radius_spinner.value = x_radius * 10
+	x_radius_spinner.max_value = x_radius
+	x_radius_spinner.value_changed.connect(update_x_radius)
+	x_radius_spinner.position.y = y_speed_spinner.position.y + 30
+	x_radius_spinner.position.x += spinner_x_offset
+	
+	y_radius_spinner.max_value = y_radius * 10
+	y_radius_spinner.value = y_radius
+	y_radius_spinner.value_changed.connect(update_y_radius)
+	y_radius_spinner.position.y = x_radius_spinner.position.y + 30
+	y_radius_spinner.position.x += spinner_x_offset
+	
+	x_radius_label.text = "X Radius:"
+	
+	x_radius_label.position.y = x_radius_spinner.position.y
+	
+	y_radius_label.text = "Y Radius:"
+	
+	y_radius_label.position.y = y_radius_spinner.position.y
+	
+	x_speed_spinner.value_changed.connect(update_cos_step)	
+	x_speed_spinner.value = x_speed
+	
+	y_speed_spinner.value_changed.connect(update_sin_step)	
+	y_speed_spinner.value = y_speed
+	
+	
+	add_child(sin_wave)
+	add_child(cos_wave)
+	add_child(y_axis)
+	add_child(x_axis)
+	add_child(obj)
+	add_child(sin_label)
+	add_child(cos_label)
+	add_child(x_speed_spinner)
+	add_child(x_speed_label)
+	add_child(y_speed_spinner)
+	add_child(y_speed_label)
+	add_child(x_radius_spinner)
+	add_child(x_radius_label)
+	add_child(y_radius_spinner)
+	add_child(y_radius_label)
+
+
+func get_sin_full_circle_2dvectors(degrees_delta: int, scale: int, number_of_phases: int) -> Array:
+	var points = []
+	var i = 0;
+	while i < number_of_phases * (2 * PI):
+		var x = sin(i)
+		points.append(Vector2(i*scale, x*scale))
+		i += deg_to_rad(degrees_delta)
+	return points
+
+func get_cos_full_circle_2dvectors(degrees_delta: int, scale: int, number_of_phases: int ) -> Array:
+	var points = []
+	var i = 0;
+	while i < number_of_phases * (2 * PI):
+		var x = cos(i)
+		points.append(Vector2(i*scale, x*scale))
+		i += deg_to_rad(degrees_delta)
+	return points
+
+
+#In this case our curve is just a simple "circle". No fancy curves yet.
+func calc_curve(speed_agle: float):
+	pass
+
+func update_sin_step(value: float):
+	x_speed = value
+
+func update_cos_step(value: float):
+	y_speed = value
+	
+func update_x_radius(value: float):
+	x_radius = value
+
+func update_y_radius(value: float):
+	y_radius = value
+
+# TODO:Draw This Ellipses
+func _physics_process(delta):
+	#In this case our curve is just a simple "ellipses". No fancy curves yet.
+	current_sinusoidal_output_val = sin(current_sin_input_val) 
+	current_cos_output_val = cos(current_cos_input_val) 
+	var old_position = Vector2(obj.position.x, obj.position.y)
+	obj.position.x = center_x 
+	obj.position.y = center_y
+	obj.position.y += (y_radius * current_sinusoidal_output_val)  
+	obj.position.x += (x_radius * current_cos_output_val)  
+	position_delta.x = obj.position.x - old_position.x
+	position_delta.y = obj.position.y - old_position.y
+
+	
+	current_sin_input_val += x_speed
+	current_cos_input_val += y_speed
+	
+func _process(delta):
+	pass
