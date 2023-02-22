5d2fef75ebd8545cf54d61a0aac94ba775afd546
thebigG <yunior.eury@gmail.com>
2023-02-21 21:07:01 -0600

    -Add eq_one_point_six to ch1.

--- godot/scenes/fundamental_physics/ch1/structure_of_mechanics.gd
+++ godot/scenes/fundamental_physics/ch1/structure_of_mechanics.gd
@@ -6,6 +6,15 @@ func _ready():
 	pass # Replace with function body.
 
 
+# y(t) = (1/2 * (a * t ^ 2)) + (b * t) + c
+# t = time in seconds
+# a = acceleration
+# b = Initial velocity
+# c = Initial position
+# Returns the position of an object at time t.
+func eq_one_point_six(t: float, a: float, b: float, c: float):
+	return (1/2 * (a  * pow(t, 2))) + (b * t) + c
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
