6b999b8c19a2c8b4d509ce3318bd65d46d3682e7
thebigG <yunior.eury@gmail.com>
2023-02-23 23:01:59 -0600

    -Add edge particle. WIP.

--- godot/scenes/edge_particles.gd
+++ godot/scenes/edge_particles.gd
@@ -14,19 +14,24 @@ var cos_wave = Line2D.new();
 var y_axis_length = 600
 var x_axis_length = 600
 
-var y_axis = Line2D.new();
-var x_axis = Line2D.new();
+var y_axis = Line2D.new()
+var x_axis = Line2D.new()
 
-var Origin = Vector2(500,200);
+
+var left_wall = Line2D.new()
+var right_wall = Line2D.new()
+
+var Origin = Vector2(700,200);
 
 var particle1_sprite = Sprite2D.new()
-var particle1 =  RstyParticle2.new(RstyVector2.new(), 10, -PI/2)
+var particle1 =  RstyParticle2.new()
 
-var particle2_sprite = Sprite2D.new()
-var particle2 =  RstyParticle2.new()
+#var particle_position = Origin
+var particle1_position = Vector2()
 
+var particle_velocity = Vector2()
 
-var particle_mass = 15000
+var particle_mass = 100
 
 var current_sinusoidal_output_val = 0
 var current_cos_output_val = 0
@@ -40,12 +45,17 @@ var cos_label = Label.new()
 var center_x = Origin.x
 var center_y = Origin.y
 
-var mass_spinner = SpinBox.new()
-var mass_spinner_label = Label.new()
+var x_velocity_spinner = SpinBox.new()
+var velocity_label = Label.new()
+
+var x_acceleration_spinner = SpinBox.new()
+var x_acceleration_label = Label.new()
 
 var radius = 100
 
 var code_link = RichTextLabel.new()
+var rsty_particle = RstyParticle2.new(RstyVector2.new(0.0,0.0), 1, PI)
+
 
 var v: RstyVector2 = RstyVector2.new(0, 0)
 
@@ -63,25 +73,15 @@ func _ready():
 	var image = Image.load_from_file("res://assets/icon.svg")
 	var texture = ImageTexture.create_from_image(image)
 	
-	var sun_image = Image.load_from_file("res://assets/sun.png")
-	var sun_texture = ImageTexture.create_from_image(sun_image)
-	
+	particle1_position = Vector2(Origin)
 	particle1_sprite.texture = texture
-	particle1.position.x = Origin.x + 200
-	particle1.position.y = Origin.y
+	particle1.position.x = particle1_position.x
+	particle1.position.y = particle1_position.y
 	particle1_sprite.position = particle1.position.get_godot_vector()
 	particle1_sprite.scale = Vector2(0.3,0.3)
-#	particle1_sprite.modulate = Color(1, 0, 0, 0.5)	
-	
-	particle2_sprite.texture = sun_texture
-	particle2.position.x = Origin.x
-	particle2.position.y = Origin.y
-	particle2_sprite.position = particle2.position.get_godot_vector()
-	particle2_sprite.scale = Vector2(0.030 * particle_mass/pow(10,4),0.030 * particle_mass/pow(10,4))
-#	particle2_sprite.modulate = Color(1, 0, 0, 0.5)
-	particle2.mass = particle_mass
 
 #	Have to do this since godot has no "set_length" for vectors. https://github.com/godotengine/godot/pull/37704
+	particle_velocity = Vector2(2,0)
 
 	y_axis.default_color = Color(Color.YELLOW)
 	y_axis.default_color.a = 0.25
@@ -92,6 +92,17 @@ func _ready():
 	x_axis.default_color.a = 0.25
 
 	x_axis.points = [Vector2(Origin.x-x_axis_length, Origin.y), Vector2(Origin.x+x_axis_length, Origin.y)]
+	
+	left_wall.default_color = Color(Color.RED)
+	left_wall.default_color.a = 0.25
+	
+	left_wall.points = [Vector2(Origin.x-x_axis_length, Origin.y-y_axis_length), Vector2(Origin.x-x_axis_length, Origin.y+y_axis_length)]
+	
+	
+	right_wall.default_color = Color(Color.RED)
+	right_wall.default_color.a = 0.25
+	
+	right_wall.points = [Vector2(Origin.x+x_axis_length, Origin.y-y_axis_length), Vector2(Origin.x+x_axis_length, Origin.y+y_axis_length)]
 
 	sin_wave.default_color = Color.PURPLE
 
@@ -106,23 +117,49 @@ func _ready():
 	cos_label.position.y += 25
 	sin_label.add_theme_color_override("font_color", sin_wave.default_color)
 
-	mass_spinner.position.y += 50
-	mass_spinner.step = 100
-	mass_spinner.min_value = 0
-	mass_spinner.max_value = abs(particle_mass * 100) 
+	x_velocity_spinner.position.y += 50
+	x_velocity_spinner.step = .01
+	x_velocity_spinner.min_value = abs(x_velocity) * -1
+	x_velocity_spinner.max_value = abs(x_velocity) 
 
-	mass_spinner_label.text = "Sun Mass:"
+	velocity_label.text = "x velocity:"
 
-	mass_spinner_label.position.y = mass_spinner.position.y
+	velocity_label.position.y = x_velocity_spinner.position.y
 
-	mass_spinner.position.x += 75
+	x_velocity_spinner.position.x += 75
+	
+	x_velocity_spinner.min_value = -1000.0
+	x_velocity_spinner.max_value = 1000.0
 
-	mass_spinner.value_changed.connect(update_mass)
-	mass_spinner.value = particle_mass
+	x_velocity_spinner.value_changed.connect(update_x_velocity)
+	x_velocity_spinner.value = x_velocity
+	
+	x_acceleration_spinner.position.y += 100
+	x_acceleration_spinner.step = 0.001
+	x_acceleration_spinner.min_value = abs(x_acceleration) * -1
+	x_acceleration_spinner.max_value = abs(x_acceleration) 
+
+	x_acceleration_label.text = "x acceleration:"
+
+	x_acceleration_label.position.y = x_acceleration_spinner.position.y
+
+	x_acceleration_spinner.position.x += 125
+	
+	x_acceleration_spinner.min_value = -1000.0
+	x_acceleration_spinner.max_value = 1000.0
+
+	x_acceleration_spinner.value_changed.connect(update_x_acceleration)
+	x_acceleration_spinner.value = x_acceleration
+	
+	apply_x_acceleration_button.text = "Press To Accelerate"
+	apply_x_acceleration_button.position.y = x_acceleration_spinner.position.y
+	apply_x_acceleration_button.position.x = x_acceleration_spinner.position.x + 100
+	apply_x_acceleration_button.button_down.connect(update_apply_x_accel)
+	apply_x_acceleration_button.button_up.connect(update_apply_x_accel)
 
 	code_link.bbcode_enabled = true
 
-	code_link.text = "[url=https://github.com/thebigG/rsty_physics/blob/main/godot/scenes/gravity_particles.gd][/https://github.com/thebigG/rsty_physics/blob/main/godot/scenes/gravity_particles.gd]"
+	code_link.text = "[url=https://github.com/thebigG/rsty_physics/blob/main/godot/velocity_particle.gd][/https://github.com/thebigG/rsty_physics/blob/main/godot/velocity_particle.gd]"
 	code_link.meta_underlined = true
 	code_link.size = Vector2(600,100)
 	code_link.position = Vector2(Origin)
@@ -130,18 +167,22 @@ func _ready():
 	code_link.size_flags_horizontal = 0
 	code_link.visible = true
 	code_link.meta_clicked.connect(open_browser_link)
+	particle1.velocity = RstyVector2.new(particle_velocity.x, particle_velocity.y)
 
 	add_child(sin_wave)
 	add_child(cos_wave)
 	add_child(y_axis)
 	add_child(x_axis)
 	add_child(particle1_sprite)
-	add_child(particle2_sprite)
 	add_child(sin_label)
 	add_child(cos_label)
-	add_child(mass_spinner)
-	add_child(mass_spinner_label)
+	add_child(x_velocity_spinner)
+	add_child(velocity_label)
+	add_child(left_wall)
+	add_child(right_wall)
 	
+	add_child(x_acceleration_spinner)
+	add_child(x_acceleration_label)
 	add_child(apply_x_acceleration_button)
 	add_child(code_link)
 
@@ -157,10 +198,17 @@ func get_vector2_with_angle(v: Vector2, angle: float) -> Vector2:
 
 
 func update_particle():
-	if(particle_mass != particle2.mass):
-		particle2.mass = particle_mass
-		particle2_sprite.scale = Vector2(0.030 * particle_mass/pow(10,4),0.030 * particle_mass/pow(10,4))
-	particle1.gravitate_to(particle2)
+	if(particle1.position.x > Origin.x  + x_axis_length or particle1.position.x < Origin.x - x_axis_length):
+		x_acceleration = -1 * x_acceleration
+		particle1.velocity.x  = -1 * particle1.velocity.x
+	
+	x_acceleration_spinner.value = x_acceleration
+
+	if(apply_x_accel):
+		particle1.accelerate(RstyVector2.new(x_acceleration, 0))
+	
+	x_velocity_spinner.value = particle1.velocity.x	
+	
 	particle1.update()
 	
 	particle1_sprite.position = particle1.position.get_godot_vector() 
@@ -176,8 +224,8 @@ func update_apply_x_accel():
 func calc_curve(speed_agle: float):
 	pass
 
-func update_mass(value: float):
-	particle_mass = value
+func update_x_velocity(value: float):
+	x_velocity = value
 
 func update_x_acceleration(value: float):
 	x_acceleration = value
